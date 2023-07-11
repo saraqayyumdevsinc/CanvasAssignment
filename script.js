e6c7672b54621f691f106b10483ef296202dceda
saraqayyumdevsinc <sara.intern@devsinc.com>
2023-07-11 13:51:40 +0500

    Feature: Create canvas

--- script.js
+++ script.js
@@ -6,4 +6,23 @@ $(document).ready(function() {
     $('.tool-item').on('mouseleave', function() {
         $(this).find('.tool-colors').css('display', 'none');
     });
+
+    var canvas = document.querySelector("canvas"); // Use querySelector instead of getElementsByTagName
+    console.log(canvas);
+    var context = canvas.getContext("2d");
+    console.log(context);
+    var boundings = canvas.getBoundingClientRect();
+
+    var mouseX = 0;
+    var mouseY = 0;
+    context.strokeStyle = 'black'; // initial brush color
+    context.lineWidth = 1; // initial brush width
+    var isDrawing = false;
+
+    // Handle Colors
+    var colors = document.getElementsByClassName('color-option');
+
+    colors.addEventListener('click', function(event) {
+    context.strokeStyle = event.target.value || 'black';
+  });
 });
